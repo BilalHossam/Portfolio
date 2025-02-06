1b4fa7d306d9687cd5a399313f3291f6e1eeddd8
BilalHossam <Kovacks97@gmail.com>
2025-02-06 21:25:21 +0200

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from app import db
-
-class Poll(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    question = db.Column(db.String(200), nullable=False)
-    option1 = db.Column(db.String(100), nullable=False)
-    option2 = db.Column(db.String(100), nullable=False)
-    votes1 = db.Column(db.Integer, default=0)
-    votes2 = db.Column(db.Integer, default=0)
